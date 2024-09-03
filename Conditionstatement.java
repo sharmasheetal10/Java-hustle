c297ebc7cfaea135787a043c9717da99109107cb
sharmasheetal01 <86783709+sharmasheetal10@users.noreply.github.com>
2024-09-03 18:49:42 +0000

    v3

---
+++ Conditionstatement.java
@@ -0,0 +1,13 @@
+public class ConditionStatement {
+    public static void main(String args[]){
+        int a =989;
+        if(a > 100){
+        System.out.print( "The staement is positive.");
+    }else if(a <100){
+        System.out.println("The statement is negative,");
+    }else{
+        System.out.println("I am happy.");
+    }
+}
+}
+    
---
+++ dataType.java
@@ -0,0 +1,10 @@
+public class dataType{
+    public static void main(String args[]){
+        int a = 20;
+        int b = 54;
+        int sum = a+b;
+        boolean iSJavaFun = true;
+        System.out.println("The update value of sum is:" + sum);
+        System.out.println("Is Java Fun?:" + isJavaFun );
+    }
+}
